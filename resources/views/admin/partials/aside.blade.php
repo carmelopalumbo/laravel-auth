ecf89c04fb903e2624669b2e3eb9a0db8777827d
Carmelo Palumbo <carmelopalumbo@outlook.it>
2023-01-18 15:29:58 +0100

    add-resource-controller

--- resources/views/admin/partials/aside.blade.php
+++ resources/views/admin/partials/aside.blade.php
@@ -1,11 +1,11 @@
-<aside class="col-2 bg-dark text-white option-box shadow-sm text-center">
+<aside class="col-2 bg-dark text-white option-box shadow-sm text-center pt-5">
     <ul>
-        <li>
-            <a href="{{ route('admin.dashboard') }}">Home</a>
+        <li class="pb-5">
+            <a href="{{ route('admin.dashboard') }}">HOME</a>
         </li>
 
-        <li>
-            <a href="{{ route('home') }}">Sito</a>
+        <li class="pb-5">
+            <a href="{{ route('admin.projects.index') }}">PROGETTI</a>
         </li>
     </ul>
 </aside>
---
+++ resources/views/admin/projects/index.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.admin')
+
+@section('content')
+    <div class="container-fluid">
+        <div class="row">
+            @include('admin.partials.aside')
+            <div class="col-10">
+                TABELLA PROGETTI
+            </div>
+        </div>
+    </div>
+@endsection
