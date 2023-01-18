828d33c28adcc4ba891ee6a4510165fe2ceab541
Carmelo Palumbo <carmelopalumbo@outlook.it>
2023-01-18 18:49:48 +0100

    fix-show

--- resources/views/admin/projects/show.blade.php
+++ resources/views/admin/projects/show.blade.php
@@ -8,15 +8,16 @@
                 <div class="card m-auto mt-5" style="width: 30rem;">
                     <img src="{{ $project->cover_image }}" class="card-img-top thumb" alt="{{ $project->name }}">
                     <div class="card-body">
-                        <h5 class="card-title text-center py-3">{{ $project->name }}</h5>
+                        <h5 class="card-title text-center py-3 fw-bold">{{ $project->name }}</h5>
                         <p class="card-text">{{ $project->summary }}</p>
                         <div class="d-flex justify-content-center py-3">
-                            <a href="{{ route('admin.projects.edit', $project) }}" class="btn btn-warning mx-3">MODIFICA</a>
+                            <a href="{{ route('admin.projects.edit', $project) }}"
+                                class="btn btn-warning mx-3 fw-bold">MODIFICA</a>
                             <form onsubmit="return confirm('Confermi l\'eliminazione di {{ $project->name }} ?')"
                                 class="d-inline" method="POST" action="{{ route('admin.projects.destroy', $project) }}">
                                 @csrf
                                 @method('DELETE')
-                                <button class="btn btn-danger" type="submit">ELIMINA</button>
+                                <button class="btn btn-danger fw-bold" type="submit">ELIMINA</button>
                             </form>
                         </div>
                     </div>
