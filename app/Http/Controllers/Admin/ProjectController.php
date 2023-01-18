c4267101a836b305ea04b41956060abb4dd8da21
Carmelo Palumbo <carmelopalumbo@outlook.it>
2023-01-18 15:52:02 +0100

    add-table

--- app/Http/Controllers/Admin/ProjectController.php
+++ app/Http/Controllers/Admin/ProjectController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Project;
 use Illuminate\Http\Request;
 
 class ProjectController extends Controller
@@ -14,7 +15,8 @@ class ProjectController extends Controller
      */
     public function index()
     {
-        return view('admin.projects.index');
+        $my_projects = Project::paginate(7);
+        return view('admin.projects.index', compact('my_projects'));
     }
 
     /**
--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -5,7 +5,37 @@
         <div class="row">
             @include('admin.partials.aside')
             <div class="col-10">
-                TABELLA PROGETTI
+                <h3 class="text-center py-4">I MIEI PROGETTI</h3>
+                <table class="table table-striped w-75 m-auto">
+                    <thead>
+                        <tr>
+                            <th scope="col">NOME</th>
+                            <th scope="col">NOME CLIENT</th>
+                            <th scope="col">AZIONI</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @forelse ($my_projects as $project)
+                            <tr>
+                                <td>{{ $project->name }}</td>
+                                <td>{{ $project->client_name }}</td>
+                                <td>
+                                    <a class="btn btn-info" href=""><i class="fa-regular fa-eye"></i></a>
+                                    <a class="btn btn-warning mx-2" href=""><i
+                                            class="fa-regular fa-pen-to-square"></i></a>
+                                    <a class="btn btn-danger" href=""><i class="fa-solid fa-trash"></i></a>
+                                </td>
+                            </tr>
+                        @empty
+                            <h4>Nessun progetto presente.</h4>
+                        @endforelse
+                    </tbody>
+                </table>
+
+                <div class="m-auto w-75 py-4">
+                    {{ $my_projects->links() }}
+                </div>
+
             </div>
         </div>
     </div>
