ed33cb0ae285afcfd3ece59b93f3f3ee96863208
Carmelo Palumbo <carmelopalumbo@outlook.it>
2023-01-19 09:25:45 +0100

    add-active-links

--- resources/views/admin/partials/aside.blade.php
+++ resources/views/admin/partials/aside.blade.php
@@ -1,6 +1,18 @@
 <aside class="col-2 bg-dark text-white option-box pt-5 container-fluid">
-    <div class="text-center links fw-bold">
-        <a class="d-block pb-5" href="{{ route('admin.dashboard') }}">HOME</a>
-        <a class="d-block" href="{{ route('admin.projects.index') }}">PROGETTI</a>
+    <div class="text-center links fw-bold container">
+
+        <p class="mb-5">
+            <a class="{{ Route::currentRouteName() === 'admin.dashboard' ? 'active' : 'notactive' }}"
+                href="{{ route('admin.dashboard') }}">HOME</a>
+        </p>
+
+        <p class="mb-5">
+            <a class="{{ Route::currentRouteName() === 'admin.projects.index' ? 'active' : 'notactive' }}"
+                href="{{ route('admin.projects.index') }}">PROGETTI</a>
+        </p>
+
+        <p>
+            <a class="notactive" target="_blank" href="https://github.com/carmelopalumbo">GITHUB</a>
+        </p>
     </div>
 </aside>
